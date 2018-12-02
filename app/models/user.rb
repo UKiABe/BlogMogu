c793eaa837648ff569eae15c65a982feed5fb891
UkiAbe <unlimited.blade.works22@gmail.com>
2018-12-02 21:35:01 +0900

    shrine実装前

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # 認証画面を表示する場合は:registerable,を追加
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 end
