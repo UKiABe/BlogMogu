eaee087c77445790127ca7b968d21683e5995e93
UkiAbe <unlimited.blade.works22@gmail.com>
2018-12-06 14:58:26 +0900

    Allow registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # 認証画面を表示する場合は:registerable,を追加
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable
          :recoverable, :rememberable, :validatable
 end
